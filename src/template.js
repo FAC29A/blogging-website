a43b1e0f3b75f85b7f3395a002f3404e53b40bdc
Lucien Lin <luciensday@gmail.com>
2024-01-23 12:53:57 +0000

    immigrate the home js  but postsjs hasnt

--- src/template.js
+++ src/template.js
@@ -1,21 +1,34 @@
 //for functions
-function home(blogPosts) {
-  const title = "Blogging Website";
+const repetitiveHtml = /*html*/ `
+<h1>Blog Posts</h1>
+<form action="/posts" method="POST">
+  <div class="input">
+    <label for="name">Name:</label><br>
+    <input name="name" type="text">
+  </div>
+  <div class="input">
+    <label for="blogpost">Type your post here:</label><br>
+    <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
+  </div class="input">
+    <button type="submit">Submit</button>
+</form>`;
+
+function home() {
+  const title = "Blogging Website Home";
+  const content =
+    /*html*/
+    `
+      ${repetitiveHtml} 
+      `;
+  return layout(title, content);
+}
+
+function posts(blogPosts) {
+  const title = "Post Page";
   const content =
     /*html*/
     ` 
-    <h1>Blog Posts</h1>
-    <form action="/posts" method="POST">
-      <div class="input">
-        <label for="name">Name:</label><br>
-        <input name="name" type="text">
-      </div>
-      <div class="input">
-        <label for="blogpost">Type your post here:</label><br>
-        <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
-      </div class="input">
-        <button type="submit">Submit</button>
-    </form>
+    ${repetitiveHtml}    
     <div class="posted-blogs">
     ${list.join("")}
     </div>
@@ -41,4 +54,4 @@ function layout(title, content) {
     `;
 }
 
-module.exports = { home };
+module.exports = { posts, home };
