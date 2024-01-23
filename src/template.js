6b85bf9fdfa965c9bd7894a094522155a4abff9d
Lucien Lin <luciensday@gmail.com>
2024-01-23 11:52:15 +0000

    refactoring html into template js

--- src/template.js
+++ src/template.js
@@ -1,1 +1,39 @@
 //for functions
+function home(blogPosts) {
+  const title = "Blogging Website";
+  const content =
+    /*html*/
+    ` 
+    <h1>Blog Here</h1>
+    <form action="/posts" method="POST">
+      <div class="input">
+        <label for="name">Name:</label><br>
+        <input name="name" type="text">
+      </div>
+      <div class="input">
+        <label for="blogpost">Type your post here:</label><br>
+        <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
+      </div class="input">
+        <button type="submit">Submit</button>
+    </form>`;
+
+  return layout(title, content);
+}
+
+function layout(title, content) {
+  return /*html*/ `
+    <!doctype html>
+    <html>
+      <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/style.css">
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+    `;
+}
+
+module.exports = { home };
