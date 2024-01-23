8878e4c42d1b1b821633b1b5dcca880be8bb90b4
Lucien Lin <luciensday@gmail.com>
2024-01-23 14:56:49 +0000

    test for POST posts route complete

--- test/server.test.js
+++ test/server.test.js
@@ -11,3 +11,17 @@ test("//route shoould return expected page ", async () => {
   const body = await response.text();
   assert.match(body, /Type your post here/);
 });
+
+test("/post route respond to POST request", async () => {
+  const app = server.listen(9876);
+  const response = await fetch("http://localhost:9876/posts", {
+    method: "POST",
+    body: "blogpost=there you go ",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+  });
+  app.close();
+
+  assert.equal(response.status, 200);
+  const body = await response.text();
+  assert.match(body, /there you go/);
+});
