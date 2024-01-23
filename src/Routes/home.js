82d8bd1725755706aa959f49ca3b2b90021b341b
Frannerz <francesstephens@hotmail.com>
2024-01-23 14:13:07 +0000

    comments removed

--- src/Routes/home.js
+++ src/Routes/home.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { home } = require("../template");
 
-// add router server routes for home
-
 router.get("/", (request, response) => {
   const body = home();
   response.send(body);
