a6f2ca1da747ac5992314d2ca9ad9ac4d5d8d782
Lucien Lin <luciensday@gmail.com>
2024-01-24 18:20:27 +0000

    ,input name and blgpost will stay in input field in case user miss field and press submit

--- src/template.js
+++ src/template.js
@@ -6,17 +6,17 @@ function home() {
     /*html*/
     `
     <h1>Blog Posts</h1>
-<form action="/posts" method="POST">
-  <div class="input">
-    <label for="name">Name:</label><br>
-    <input name="name" type="text">
-  </div>
-  <div class="input">
-    <label for="blogpost">Type your post here:</label><br>
-    <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
-  </div class="input">
-    <button type="submit">Submit</button>
-</form>
+    <form action="/posts" method="POST">
+        <div class="input">
+            <label for="name">Name:</label><br>
+            <input name="name" type="text">
+        </div>
+        <div class="input">
+            <label for="blogpost">Type your post here:</label><br>
+            <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
+        </div class="input">
+        <button type="submit">Submit</button>
+    </form>
       `;
   return layout(title, content);
 }
@@ -30,12 +30,16 @@ function posts(blogPosts, errorsObject = {}, requestBody = {}) {
     <form action="/posts" method="POST">
         <div class="input">
             <label for="name">Name:</label><br>
-            <input name="name" type="text">
+            <input name="name" type="text" value="${
+              requestBody.name ? sanitize(requestBody.name) : ""
+            }">
             ${validation(errorsObject.nameError)}
         </div>
         <div class="input">
             <label for="blogpost">Type your post here:</label><br>
-            <textarea name="blogpost" type="textarea" rows="4" columns="50"></textarea>
+            <textarea name="blogpost" type="textarea" rows="4" columns="50">${
+              requestBody.blogpost ? sanitize(requestBody.blogpost) : ""
+            }</textarea>
             ${validation(errorsObject.postError)}
         </div class="input">
         <button type="submit">Submit</button>
