48c7b995e2c23432cfa31716041c5ed7a43766e7
Lucien Lin <luciensday@gmail.com>
2024-01-29 20:57:19 +0000

    move inddexjs to the root repository

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require("./server");
+const server = require("./src/server");
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
