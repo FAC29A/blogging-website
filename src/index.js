4fd2b5b083008a4e89b381d52c576cd88ba4c17d
Lucien Lin <luciensday@gmail.com>
2024-01-29 19:43:45 +0000

    temporary change listen to 300 without port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const server = require("./server");
-const PORT = process.env.PORT || 3000;
+// const server = require("./server");
+// const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+// server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
+const server = require("./server");
+
+server.listen(3000, () => console.log("Listening at http://localhost:3000"));
