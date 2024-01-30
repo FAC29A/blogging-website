8e3833a617d820fd972d72471aa35d80d1a0eaa9
Frannerz <francesstephens@hotmail.com>
2024-01-30 15:23:30 +0000

    likes added to schema

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS blog_posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     blog_post TEXT,
-    created_at DATETIME
+    created_at DATETIME,
+    likes INTEGER DEFAULT 0
 )
 
 COMMIT;
