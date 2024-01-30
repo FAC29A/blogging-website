ada228cc28f4f9560e2bdc0bd9eb800770d557a8
Frannerz <francesstephens@hotmail.com>
2024-01-30 21:46:15 +0000

    mistake correcting and server routes

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const { readFileSync } = require("node:fs");
-const { join } = requite("node:path");
+const { join } = require("node:path");
 
 const Database = require("better-sqlite3");
 const db = new Database(process.env.DB_FILE);
@@ -8,7 +8,5 @@ const schemaPath = join("database", "schema.sql");
 const schema = readFileSync(schemaPath, "utf-8");
 db.exec(schema);
 
-
-//hello!!
 
 module.exports = db;
--- model/blogs.js
+++ model/blogs.js
@@ -3,18 +3,18 @@ const db = require("../database/db")
 //create functions here:
   
 const create_blog = db.prepare(/*sql*/ `
-    INSERT into blog_posts (name, blog_post)
-    VALUES($name, $blogPost)
-    RETURNING name, blog_post, created_at, likes
+    INSERT into blog_posts (name, blogpost)
+    VALUES($name, $blogpost)
+    RETURNING name, blogpost, created_at, likes
 `)
 
 function createBlog (blog) {
-    return createBlog.get(blog)
+    return create_blog.get(blog)
 }
 
 //read functions here:
 const read_blogs = db.prepare(/*sql*/ `
-    SELECT name, blog_post, created_at, likes
+    SELECT name, blogpost, created_at, likes
     FROM blog_posts
 `)
 
